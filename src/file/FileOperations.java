6c8a9d24cca5facecf910097b84466dfdb12d54c
ChuncI-Iunmaru <pwalczak796@gmail.com>
2019-05-21 21:05:58 +0200

    Catching exceptions from file reading

--- src/file/FileOperations.java
+++ src/file/FileOperations.java
@@ -1,6 +1,8 @@
 package file;
 
 import dictionary.Dictionary;
+import javafx.scene.control.Alert;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,32 +15,45 @@ public class FileOperations {
             File file = new File("dictionary.txt");
             Scanner in = new Scanner(file);
             while (in.hasNextLine()) {
-                String word = in.nextLine();
-                String translationWord = in.nextLine();
+                String word;
+                String translationWord;
+                word = in.nextLine();
+                //Żeby uniknąć wyjątku wczytywania nieistniejącej następnej linii
+                if (in.hasNextLine()) {
+                    translationWord = in.nextLine();
+                } else throw new IllegalArgumentException("Nie ma następnej linii!");
+                System.out.println(word+" - "+translationWord);
+                //Problem z dodawaniem słowa do Eng - paramter "word" powoduje "Niedozwolony znak w wyrazie"
+                //Nawet przy parzystej liczbie wyrazów? Tak długo jak po usunięciu pliku?
                 Dictionary.getEngDictionary().insert(word, translationWord);
                 Dictionary.getPolishDictionary().insert(translationWord, word);
             }
-
-
-
+        } catch (IllegalArgumentException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Komunikat");
+            alert.setHeaderText("Brakujący wyraz w pliku słownika!");
+            alert.showAndWait();
         } catch (Exception e) {
             e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Komunikat");
+            alert.setHeaderText("Błąd wczytywania pliku słownika!");
+            alert.showAndWait();
         }
     }
 
 
-        public static void saveDicitionary(String ang, String pol) throws IOException {
+    public static void saveDicitionary(String ang, String pol) throws IOException {
 
-            FileWriter fileWriter = new FileWriter("dictionary.txt",true);
-            PrintWriter printWriter = new PrintWriter(fileWriter);
-            printWriter.print("\n");
-            printWriter.print(ang);
-            printWriter.print("\n");
-            printWriter.print(pol);
-            printWriter.close();
+        FileWriter fileWriter = new FileWriter("dictionary.txt", true);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        printWriter.print("\n");
+        printWriter.print(ang);
+        printWriter.print("\n");
+        printWriter.print(pol);
+        printWriter.close();
 
-        }
-
+    }
 
 
 }
